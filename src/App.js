cd616179e4d015fb92ee2811c8493878013207a6
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-05-01 09:37:37 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold text-red-500">Hello world!</h1>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,24 @@
+/* eslint-disable react/jsx-no-undef */
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 p-6 w-10/12">
+      <div className="flex-1">
+        <h3 className="text-3xl font-semibold">Project Management</h3>
+      </div>
+      <div className="flex-none gap-6 ">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+        <button className="btn bg-blue-700">Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
